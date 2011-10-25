7cd035190571748aad4837b45c7936a1ed794d30
Steve Raden <radlab773@gmail.com>
2011-10-24 19:16:26 -0500

    Blank Rails Appplication

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhotoContest::Application.config.secret_token = '7bc1d72e7e2d121e5bfbf61376d9d6e29434c1ac762ae937f3542fe5e9fed160e7e42de44cf5ff460febf47730134adba997b6b63166fe19f0ca0c32ba2ed7cf'
