f0166516482697ec63664fbb61dd18aa9ef1d507
Steve Raden <radlab773@gmail.com>
2011-11-04 14:03:58 -0500

    branding on customer facing pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,16 @@
 module ApplicationHelper
   
   def get_brand
-    return "USAgencies"
+    return PhotoContest::Application.config.brand_name
   end
   
   def get_brand_url
      #return link_to(get_brand, "www.usagencies.com")
-     return 
+     return PhotoContest::Application.config.brand_url     
+  end
+  
+  def get_branded_asset (asset_name)
+     return PhotoContest::Application.config.brand_dir + "/" + asset_name    
   end
   
   def get_month_if_rated (entry)
---
+++ config/initializers/brand_parameters.rb
@@ -0,0 +1,45 @@
+ 
+#change this configuration element to change the brand
+PhotoContest::Application.config.brand_name = "US"
+# not this is an initalizer, rails app restart is required to reflect changes
+
+if PhotoContest::Application.config.brand_name == "AA"
+#A-Affordable
+  PhotoContest::Application.config.brand_name = "A-Affordable"
+  PhotoContest::Application.config.brand_dir = "AA"
+  PhotoContest::Application.config.brand_url  = "http://www.a-affordableinsurance.com/"
+  PhotoContest::Application.config.header_flash_asset_path  = "/videos/AA/affirmative_flash_aa.swf"
+  PhotoContest::Application.config.header_offerbox_asset_path  = "AA/offerbox.png"
+  PhotoContest::Application.config.logo_asset_path  = "AA/aalogo.png"
+  
+elsif PhotoContest::Application.config.brand_name == "IO"
+  #InsureOne
+  PhotoContest::Application.config.brand_name = "InsureOne"
+  PhotoContest::Application.config.brand_dir = "IO"
+  PhotoContest::Application.config.brand_url  = "http://www.insureone.com/"
+  PhotoContest::Application.config.header_flash_asset_path  = "/videos/IO/affirmative_flash_io.swf"
+  PhotoContest::Application.config.header_offerbox_asset_path  = "IO/offerbox.png"
+  PhotoContest::Application.config.logo_asset_path  = "IO/iologo.png"
+  
+elsif PhotoContest::Application.config.brand_name == "DC"
+  PhotoContest::Application.config.brand_name = "DriversChoice"
+  PhotoContest::Application.config.brand_dir = "DC"
+  PhotoContest::Application.config.brand_url  = "http://www.driverschoiceinsurance.com/"
+  PhotoContest::Application.config.header_flash_asset_path  = "/videos/DC/affirmative_flash_dc.swf"
+  PhotoContest::Application.config.header_offerbox_asset_path  = "DC/offerbox.png"
+  PhotoContest::Application.config.logo_asset_path  = "DC/DClogo.png"
+  
+elsif PhotoContest::Application.config.brand_name == "US"
+  PhotoContest::Application.config.brand_name = "USAgencies"
+  PhotoContest::Application.config.brand_dir = "US"
+  PhotoContest::Application.config.brand_url  = "http://www.USAgencies.com/"
+  PhotoContest::Application.config.header_flash_asset_path  = "/videos/US/affirmative_flash.swf"
+  PhotoContest::Application.config.header_offerbox_asset_path  = "US/offerbox.png"
+  PhotoContest::Application.config.logo_asset_path  = "US/usalogo.png"
+  
+else
+  raise "Brand is not configured"
+  
+end
+
+
