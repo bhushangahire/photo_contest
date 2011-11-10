519acf1404e35bdca8c0bb4e285f47c9595f1527
Steve Raden <radlab773@gmail.com>
2011-11-10 01:29:47 -0600

    better brand parameters

--- app/mailers/contestant_mailer.rb
+++ app/mailers/contestant_mailer.rb
@@ -2,8 +2,8 @@ class ContestantMailer < ActionMailer::Base
   default from: "mail@picthiscontest.com"
   
   def thank_you_email(contestant)
-    @brand_name = PhotoContest::Application.config.brand_name = "A-Affordable"
-    @brand_url  = PhotoContest::Application.config.brand_dir = "AA"        
+    @brand_name = PhotoContest::Application.config.brand_name 
+    @brand_url  = PhotoContest::Application.config.brand_url
     #@url = "http://example.com/login"  #do we need to this set this?
     mail(:to =>  contestant.email,  
          :subject => "Got it! Your photo has been received.")
